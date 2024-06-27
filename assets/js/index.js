f9174829ce3267bfa427a6efbc0acfa54308af25
yrkims <kyl7809@gmail.com>
2024-06-27 16:25:09 +0900

    profile edit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    new fullpage('#fullpage', {
+        // 옵션을 추가합니다.
+        autoScrolling: true,
+        navigation: true,
+    });
+});
--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    new fullpage('#fullpage', {
-        // 옵션을 추가합니다.
-        autoScrolling: true,
-        navigation: true,
-    });
-});
